b3035b392801b288129352ef637ca39da5b010c0
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-01-16 10:52:43 +0530

    Add better error catching

--- script.js
+++ script.js
@@ -3,50 +3,58 @@ import { DownloaderHelper } from 'node-downloader-helper';
 import fs from 'fs';
 
 const main = async (course) => {
-    fetch('http://125.22.54.221:8080/jspui/browse?type=author&value=' + course + "&sort_by=1&order=ASC&rpp=100&etal=0&submit_browse=Update")
-    .then(res => res.text())
-    .then(code => {
-        const matches = code.toString().match(/<td headers="t2" ><a href="(.*?)"/g);
-        matches.forEach(async link => {
-            fetch('http://125.22.54.221:8080' + link.split(`<a href="`)[1].split(`"`)[0])
-            .then(res => res.text())
-            .then(content => {
-                const year = content.split(`name="citation_date" content=`)[1].split(`"`)[1];
-                if(!fs.existsSync("PYQs_" + course + "/" + year)){
-                    fs.mkdirSync("PYQs_" + course + "/" + year);
-                }else{
-                    const name = "PYQs_" + course + "/" + year;
-                    let i = 0;
-                    while(fs.existsSync(name + "_" + i)){
-                        i++;
-                    }
-                    fs.mkdirSync(name + "_" + i);
-                }
-                const contentMatches = content.match(/<a target="_blank" href="(.*?)"/g);
-                contentMatches.forEach(link => {
-                    const downloadLink = ('http://125.22.54.221:8080' + link.split(`href="`)[1].split(`"`)[0]);
-                    if(downloadLink.toLowerCase().includes(".pdf") || downloadLink.toLowerCase().includes(".rar") || downloadLink.toLowerCase().includes(".doc") || downloadLink.toLowerCase().includes(".docx")){
-                        const dl = new DownloaderHelper(downloadLink, './' + "PYQs_" + course + "/" + year + '/');
+    const url = encodeURI('http://125.22.54.221:8080/jspui/browse?type=author&value=' + (course) + "&sort_by=1&order=ASC&rpp=100&etal=0&submit_browse=Update");
+    fetch(url)
+        .then(res => res.text())
+        .then(code => {
+            const matches = code.toString().match(/<td headers="t2" ><a href="(.*?)"/g);
+            if (matches == null){
+                console.log(url);
+                console.log("No matches found");
+                fs.rmdirSync("PYQs_" + course);
+            }
+            else {
+                matches.forEach(async link => {
+                    fetch('http://125.22.54.221:8080' + link.split(`<a href="`)[1].split(`"`)[0])
+                        .then(res => res.text())
+                        .then(content => {
+                            const year = content.split(`name="citation_date" content=`)[1].split(`"`)[1];
+                            if (!fs.existsSync("PYQs_" + course + "/" + year)) {
+                                fs.mkdirSync("PYQs_" + course + "/" + year);
+                            } else {
+                                const name = "PYQs_" + course + "/" + year;
+                                let i = 0;
+                                while (fs.existsSync(name + "_" + i)) {
+                                    i++;
+                                }
+                                fs.mkdirSync(name + "_" + i);
+                            }
+                            const contentMatches = content.match(/<a target="_blank" href="(.*?)"/g);
+                            contentMatches.forEach(link => {
+                                const downloadLink = ('http://125.22.54.221:8080' + link.split(`href="`)[1].split(`"`)[0]);
+                                if (downloadLink.toLowerCase().includes(".pdf") || downloadLink.toLowerCase().includes(".rar") || downloadLink.toLowerCase().includes(".doc") || downloadLink.toLowerCase().includes(".docx")) {
+                                    const dl = new DownloaderHelper(downloadLink, './' + "PYQs_" + course + "/" + year + '/');
 
-                        dl.on('end', () => console.log('Download Completed'));
-                        dl.on('error', (err) => console.log('Download Failed', err));
-                        dl.start().catch(err => console.error(err));
-                    }
-                    else{
-                        console.log("Unsupported File Type: " + downloadLink);
-                    }
-                });
-            })
-            .catch(err => console.log(err))
+                                    dl.on('end', () => console.log('Download Completed'));
+                                    dl.on('error', (err) => console.log('Download Failed', err));
+                                    dl.start().catch(err => console.error(err));
+                                }
+                                else {
+                                    console.log("Unsupported File Type: " + downloadLink);
+                                }
+                            });
+                        })
+                        .catch(err => console.log(err))
+                })
+            }
         })
-    })
-    .catch(err => console.log(err));
+        .catch(err => console.log(err));
 }
 
 const course = process.argv.slice(2).join('+').toLocaleUpperCase();
-if(!course){
+if (!course) {
     console.log("Error: Run script with arguments")
-}else{
+} else {
     console.log("Downloading " + course)
     fs.mkdirSync("PYQs_" + course)
     await main(course);
